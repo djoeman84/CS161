784b8a5463a7c54779ff8916c33ac07523e50029
djoeman84 <djoeman84@gmail.com>
2014-06-01 03:24:37 -0700

    upper and lower now single int

--- CLCSFast.java
+++ CLCSFast.java
@@ -299,11 +299,18 @@ class Blacklist {
     NORMAL, TRANSPOSE
   }
 
-  private static final int NUM_BOUNDS = 2;
-  private static final int ABOVE_INDEX = 0;
-  private static final int BELOW_INDEX = 1;
+  public static enum BlacklistBound {
+    ABOVE, BELOW
+  }
 
-  private int[][][]   blacklist_bounds;
+  private static final int ABOVE_SHIFT = 0;
+  private static final int BELOW_SHIFT = 12; // far greater than the max size
+                                                  // of m
+
+  private static final int BELOW_MASK = (-1)^((1<<BELOW_SHIFT) - 1);
+  private static final int ABOVE_MASK = ((1<<BELOW_SHIFT) - 1);
+
+  private int[][] blacklist_bounds;
   private int size_m;
   private int size_n;
   private int size_nodes_m;
@@ -323,13 +330,13 @@ class Blacklist {
       {
         /* +1 since it is a node map, not an entry map */
         blacklist_bounds = new int[this.size_nodes_m]
-                          [this.size_nodes_n][NUM_BOUNDS];
+                          [this.size_nodes_n];
       }
     else
       {
         /* +1 since it is a node map, not an entry map */
         blacklist_bounds = new int[this.size_nodes_n]
-                          [this.size_nodes_m][NUM_BOUNDS];
+                          [this.size_nodes_m];
       }
   }
 
@@ -339,12 +346,14 @@ class Blacklist {
 
       for (int row = 0; row < this.size_nodes_m; row++) {
         for (int col = 0; col < this.size_nodes_n; col++) {
-          if (getIsInitialized(row, col, 0))
+          if (isInitialized(row, col, 0))
             {
               format += "\t(";
-              format += Integer.toString(getBound(row, col, 0, ABOVE_INDEX));
+              format += Integer.toString(getBound(row, col, 0, 
+                                                        BlacklistBound.ABOVE));
               format += ",";
-              format += Integer.toString(getBound(row, col, 0, BELOW_INDEX));
+              format += Integer.toString(getBound(row, col, 0, 
+                                                        BlacklistBound.BELOW));
               format += ")";
             }
           else
@@ -362,41 +371,47 @@ class Blacklist {
   public void blacklist (int row, int col, int offset_y) {
     if (isValidIndex (row, col, offset_y))
       {
-        if (!getIsInitialized(row, col, offset_y))
+        if (!isInitialized(row, col, offset_y))
           {
-            setBound(row, col, offset_y, ABOVE_INDEX);
-            setBound(row, col, offset_y, BELOW_INDEX);
+            setBound(row, col, offset_y, BlacklistBound.ABOVE);
+            setBound(row, col, offset_y, BlacklistBound.BELOW);
           }
         else 
           {
             /* lower bound */
-            if (offset_y < getBound(row, col, offset_y, ABOVE_INDEX))
-              setBound(row, col, offset_y, ABOVE_INDEX);
+            if (offset_y < getBound(row, col, offset_y, BlacklistBound.ABOVE))
+              setBound(row, col, offset_y, BlacklistBound.ABOVE);
 
             /* upper bound */
-            if (offset_y > getBound(row, col, offset_y, BELOW_INDEX))
-              setBound(row, col, offset_y, BELOW_INDEX);
+            if (offset_y > getBound(row, col, offset_y, BlacklistBound.BELOW))
+              setBound(row, col, offset_y, BlacklistBound.BELOW);
           }
       }
   }
 
-  private int getBound (int row, int col, int offset_y, int bound) {
-    return this.btype == BlacklistType.NORMAL ? 
-            blacklist_bounds[row + offset_y][col][bound] - 1:
-            blacklist_bounds[col][row + offset_y][bound] - 1;
+
+  private int getBound (int row, int col, int offset_y, BlacklistBound b) {
+    int shift = (b == BlacklistBound.ABOVE) ? ABOVE_SHIFT : BELOW_SHIFT;
+    int r = (this.btype == BlacklistType.NORMAL) ? (row + offset_y) : col;
+    int c = (this.btype == BlacklistType.NORMAL) ? col : (row + offset_y);
+
+    return ((blacklist_bounds[r][c] >> shift) & ABOVE_MASK) - 1;
   }
 
-  private void setBound (int row, int col, int offset_y, int bound) {
-    if (this.btype == BlacklistType.NORMAL)
-      blacklist_bounds[row + offset_y][col][bound] = offset_y + 1;
-    else
-      blacklist_bounds[col][row + offset_y][bound] = offset_y + 1;
+  private void setBound (int row, int col, int offset_y, BlacklistBound b) {
+    int shift = (b == BlacklistBound.ABOVE) ? ABOVE_SHIFT : BELOW_SHIFT;
+    int mask  = (b == BlacklistBound.ABOVE) ? BELOW_MASK  : ABOVE_MASK;
+    int r = (this.btype == BlacklistType.NORMAL) ? (row + offset_y) : col;
+    int c = (this.btype == BlacklistType.NORMAL) ? col : (row + offset_y);
+
+    blacklist_bounds[r][c] &= mask; // clear out old values
+    blacklist_bounds[r][c] |= ((offset_y + 1) << shift); // set new
   }
 
-  private boolean getIsInitialized (int row, int col, int offset_y) {
+  private boolean isInitialized (int row, int col, int offset_y) {
     return this.btype == BlacklistType.NORMAL ?
-            blacklist_bounds[row + offset_y][col][0] != 0 :
-            blacklist_bounds[col][row + offset_y][0] != 0;
+            blacklist_bounds[row + offset_y][col] != 0 :
+            blacklist_bounds[col][row + offset_y] != 0;
   }
 
   private boolean isValidIndex (int row, int col, int offset_y) {
@@ -424,23 +439,23 @@ class Blacklist {
 
   private int getBelowBoundPathNum (int row, int col, int offset_y) {
     /* iterate over all higher bits, check if bits are on, return on index */ 
-    if (!getIsInitialized(row, col, 0))
+    if (!isInitialized(row, col, 0))
       return -1;
-    if (getBound(row, col, 0, ABOVE_INDEX) > offset_y)
-      return getBound(row, col, 0, ABOVE_INDEX);
-    if (getBound(row, col, 0, BELOW_INDEX) > offset_y)
-      return getBound(row, col, 0, BELOW_INDEX);
+    if (getBound(row, col, 0, BlacklistBound.ABOVE) > offset_y)
+      return getBound(row, col, 0, BlacklistBound.ABOVE);
+    if (getBound(row, col, 0, BlacklistBound.BELOW) > offset_y)
+      return getBound(row, col, 0, BlacklistBound.BELOW);
     return -1;
   }
 
   private int getAboveBoundPathNum (int row, int col, int offset_y) {
     /* iterate over all lower bits, check if bits are on in any */
-    if (!getIsInitialized(row, col, 0))
+    if (!isInitialized(row, col, 0))
       return -1;
-    if (getBound(row, col, 0, ABOVE_INDEX) < offset_y)
-      return getBound(row, col, 0, ABOVE_INDEX);
-    if (getBound(row, col, 0, BELOW_INDEX) < offset_y)
-      return getBound(row, col, 0, BELOW_INDEX);
+    if (getBound(row, col, 0, BlacklistBound.ABOVE) < offset_y)
+      return getBound(row, col, 0, BlacklistBound.ABOVE);
+    if (getBound(row, col, 0, BlacklistBound.BELOW) < offset_y)
+      return getBound(row, col, 0, BlacklistBound.BELOW);
     return -1;
   }
 }
